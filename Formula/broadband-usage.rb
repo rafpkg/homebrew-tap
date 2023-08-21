a9421dee013ee82c77ac96a4fd507502a8b90de5
raf <raf@raf.org>
2023-08-21 22:29:26 +1000

    broadband-usage - Fix desc

--- Formula/broadband-usage.rb
+++ Formula/broadband-usage.rb
@@ -1,5 +1,5 @@
 class BroadbandUsage < Formula
-  desc "SMTP client with a sendmail-compatible wrapper"
+  desc "Home broadband usage meter for SNMP-enabled modem/routers"
   homepage "https://raf.org/broadband-usage/"
   url "https://github.com/raforg/broadband-usage/releases/download/v0.2/broadband-usage-0.2.tar.gz"
   sha256 "6a1b215c0d86259ee8fb250b11c436eae0b9429fe4b617465987e798d033c0c7"
